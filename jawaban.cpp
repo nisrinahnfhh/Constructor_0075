d35a04a9d1d1e1bd64de55edd9958b1e9ba63327
Nisrina Hanifah Ramadhani <nisrinahnfhh@gmail.com>
2025-05-13 15:41:20 +0700

    class

---
+++ jawaban.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+class BangunDatar;
+class PersegiPanjang{
+    public:
+       void inputData(BangunDatar &bd);
+       void outputData(BangunDatar &bd);
+};
